cc634544cc3483f1e056726139b4b3816e190264
Joe Francis <jafrancis@comcast.net>
2020-03-18 14:02:22 -0400

    AlternateFuelType

--- BusinessAssociates.Domain/Enums/AlternateFuelType.cs
+++ BusinessAssociates.Domain/Enums/AlternateFuelType.cs
@@ -2,5 +2,14 @@
 {
     public enum AlternateFuelType
     {
+        Electricity,
+        Wood,
+        Coal,
+        Propane,
+        Number2Oil,
+        Number4Oil,
+        Number5Oil,
+        Number6Oil,
+        Other
     }
 }
